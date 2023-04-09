6de34b45ce18c4038de177eb14aeee94a9c7375e
nox <dark.tempus@gmail.com>
2023-04-09 12:23:14 -0500

    follow Twitter

---
+++ projects/hello/src/App.jsx
@@ -0,0 +1,43 @@
+import './css/index.css'
+import { TwitterFollowCard } from './TwitterFollowCard.jsx'
+
+const users = [
+  {
+    userName: 'noxopio',
+    name: 'David Aguirre',
+    isFollowing: true
+  },
+  {
+    userName: 'pheralb',
+    name: 'Pablo H.',
+    isFollowing: false
+  },
+  {
+    userName: 'PacoHdezs',
+    name: 'Paco Hdez',
+    isFollowing: true
+  },
+  {
+    userName: 'TMChein',
+    name: 'Tomas',
+    isFollowing: false
+  }
+]
+
+export function App () {
+  return (
+    <section className='App'>
+      {
+        users.map(({ userName, name, isFollowing }) => (
+          <TwitterFollowCard
+            key={userName}
+            userName={userName}
+            initialIsFollowing={isFollowing}
+          >
+            {name}
+          </TwitterFollowCard>
+        ))
+      }
+    </section>
+  )
+}
---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+import React from "httpps/esm.sh/react@18.2.0"
+
+import ReactDOM from "https://esm.sh/react-dom@18.2.0/client"
+
+
+const appDomElement= document.getElementById('app')
+const root = ReactDOM.createRoot(appDomElement)
+root.render('yei')
+
+
